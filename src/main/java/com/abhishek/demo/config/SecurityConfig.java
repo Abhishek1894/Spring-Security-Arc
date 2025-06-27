e9844fe2650abbdbe477af655f7771df27e2f1d5
abhishek-ainapur1894 <abiainapur24@gmail.com>
2025-06-27 08:40:45 +0530

    Basic user password authentication done using postreges database

--- src/main/java/com/abhishek/demo/config/SecurityConfig.java
+++ src/main/java/com/abhishek/demo/config/SecurityConfig.java
@@ -1,26 +1,29 @@
 package com.abhishek.demo.config;
 
 
+import com.abhishek.demo.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.webauthn.management.UserCredentialRepository;
-
-import java.net.http.HttpRequest;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig
 {
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception
     {
@@ -44,13 +47,26 @@ public class SecurityConfig
     }
 
 
-    @Bean
-    public UserDetailsService userDetails()
-    {
-        UserDetails user1 = User.withDefaultPasswordEncoder().username("aniket").password("aniket@1234").roles("USER").build();
-        UserDetails user2 = User.withDefaultPasswordEncoder().username("harsh").password("harsh@1234").roles("USER").build();
+//    @Bean
+//    public UserDetailsService userDetails()
+//    {
+//        UserDetails user1 = User.withDefaultPasswordEncoder().username("aniket").password("aniket@1234").roles("USER").build();
+//        UserDetails user2 = User.withDefaultPasswordEncoder().username("harsh").password("harsh@1234").roles("USER").build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
 
-        return new InMemoryUserDetailsManager(user1, user2);
+    public AuthenticationManager authManager()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder()); // Using NoOp
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();  // ⚠️ Only for test/demo
     }
 
 }
