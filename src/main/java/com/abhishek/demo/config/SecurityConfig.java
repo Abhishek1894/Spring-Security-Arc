d9dead134dd8689a8dad9b676534bcb1a2c3c985
abhishek-ainapur1894 <abiainapur24@gmail.com>
2025-06-27 07:09:45 +0530

    configuring custom security filter chain

---
+++ src/main/java/com/abhishek/demo/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.abhishek.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpRequest;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception
+    {
+        // used to disable csrf token
+        security.csrf(customizer -> customizer.disable());
+
+        // ensures user is authenticated before using any resource
+        security.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+
+        // provides form login feature for authentication
+        security.formLogin(Customizer.withDefaults());
+
+        // provides authentication using headers, with this line we can fire api from postman using username and password
+        security.httpBasic(Customizer.withDefaults());
+
+        // makes session policy stateless
+        security.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // builds and returns security filter chain with above customized filters
+        return security.build();
+    }
+}
